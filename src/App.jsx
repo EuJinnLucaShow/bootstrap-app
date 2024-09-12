5a5907ab5ad9ef3d4b734846d389102a9ba55442
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-12 10:38:28 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,6 @@ function App() {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* Mobile menu button*/}
             <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
@@ -78,7 +77,6 @@ function App() {
               <BellIcon aria-hidden="true" className="h-6 w-6" />
             </button>
 
-            {/* Profile dropdown */}
             <Menu as="div" className="relative ml-3">
               <div>
                 <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -86,7 +84,7 @@ function App() {
                   <span className="sr-only">Open user menu</span>
                   <img
                     alt=""
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    src="https://res.cloudinary.com/di9eq4uza/image/upload/v1726126579/photo_youbmv.jpg"
                     className="h-8 w-8 rounded-full"
                   />
                 </MenuButton>
